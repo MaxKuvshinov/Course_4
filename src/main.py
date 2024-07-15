7c82414d1c890e8edfe29a3b2147f965a7d2700d
Max Kuvshinov <maksim.kuvshinov94@gmail.com>
2024-07-15 18:11:42 +0300

    final version

--- src/main.py
+++ src/main.py
@@ -9,8 +9,30 @@ class Product:
     def __init__(self, name: str, description: str, price: float, quantity: int):
         self.name = name
         self.description = description
-        self.price = price
+        self._price = price
         self.quantity = quantity
+
+    @property
+    def price(self) -> float:
+        return self._price
+
+    @price.setter
+    def price(self, value: float) -> None:
+        if value <= 0:
+            print("Цена введена некорректная.")
+        elif value < self._price:
+            user_confirm = input(f"Цена снижается с {self._price} до {value}. Вы уверены? (y/n): ").strip().lower()
+            if user_confirm == "y":
+                self._price = value
+                print(f"Цена успешно снижена до {value}")
+            else:
+                print("Действие отменено")
+        else:
+            self._price = value
+
+    @classmethod
+    def creates_product(cls, name: str, description: str, price: float, quantity: int) -> "Product":
+        return cls(name, description, price, quantity)
 
 
 class Category:
@@ -22,12 +44,19 @@ class Category:
     def __init__(self, name: str, description: str):
         self.name = name
         self.description = description
-        self.products = []
+        self.__products: List[Product] = []
         Category.total_categories += 1
 
     def add_product(self, product: Product) -> None:
-        self.products.append(product)
+        self.__products.append(product)
         Category.total_unique_products += 1
+
+    @property
+    def products(self) -> List[str]:
+        return [f"{product.name}, {product.price} руб. Остаток: {product.quantity} шт." for product in self.__products]
+
+    def get_product(self) -> List[Product]:
+        return self.__products
 
     @classmethod
     def get_total_categories(cls) -> int:
@@ -65,13 +94,15 @@ def load_json_file(file_path: str) -> List[Category]:
 # if __name__ == "__main__":
 #     file_path = os.path.abspath(os.path.join(os.path.dirname(__file__), "../products.json"))
 #     categories = load_json_file(file_path)
-#     for category in categories:
-#         print(f"Категория: {category.name},\nОписание: {category.description}")
-#         for product in category.products:
-#             print(
-#                 f"Название товара: {product.name},\nОписание: {product.description},\nЦена: {product.price},"
-#                 f"\nКоличество: {product.quantity}"
-#             )
 #
-#     print(f"Всего категорий: {Category.get_total_categories()}")
-#     print(f"Количество уникальных категорий: {Category.get_total_unique_products()}")
+#     for category in categories:
+#         print(f"Категория: {category.name}")
+#         print(f"Описание: {category.description}")
+#         for product_info in category.products:
+#             print(product_info)
+#         print()
+#
+#     smartphone = categories[0].get_product()[0]
+#     print(f"Текущая цена смартфона: {smartphone.price}")
+#     smartphone.price = 14000
+#     print(f"Обновленная цена смартфона: {smartphone.price}")
